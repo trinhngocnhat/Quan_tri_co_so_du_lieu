b00f56dc63e2643237f4f350d427e3ecd32a9e03
Trịnh Ngọc Nhất <trinhngocnhat079@gmail.com>
2024-11-21 08:02:05 +0700

    1

--- app.js
+++ app.js
@@ -29,5 +29,5 @@ app.post('/submit_form', (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
